2d32a0629d4e6ff205ea311c7e46f5540918ad2d
Felipe Carrion <af.carrion@uniandes.edu.co>
2020-09-15 01:29:26 -0500

    Se Ajustan barras, se adiciona evento al pasar el mouse y se genera titulo ocn el valor exacto de cada barra

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const _urlData = "https://www.datos.gov.co/resource/sfav-4met.json"
 // margenes
 
 width = 620,
-    height = 400
+height = 300
 var margin = {
     top: 50,
     right: 50,
@@ -48,6 +48,7 @@ var generateViz = (data) => {
     x = d3.scaleBand()
         .domain(data.map(d => d.mes))
         .rangeRound([margin.left, width - margin.right])
+
     y = d3.scaleLinear()
         .domain([(d3.max(data, d => +d.valor)), (d3.min(data, d => +d.valor) - 100)]) //por que se pone max-min
         .range([margin.top, height - margin.bottom])
@@ -56,10 +57,12 @@ var generateViz = (data) => {
 
     yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y));
+
     xAxis = g => g
         .attr("transform", `translate(0,${height-margin.bottom})`)
         .call(d3.axisBottom(x))
+
 
     // dibujar las barras
     svg = d3.select(".dataviz > svg")
@@ -71,11 +74,14 @@ var generateViz = (data) => {
         .selectAll("rect")
         .data(d => d)
         .join("rect")
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut) 
         .attr("y", d => y(d.valor))
-        .attr("x", (d) => x(d.mes))
+        .attr("x", (d) => 15 + x(d.mes))
         .attr("height", d => y(d3.min(data, d => +d.valor)-100) - y(+d.valor))
         .attr("width", 20)
         //.call(enter => enter.transition(t))
+
         svg.selectAll('.xaxis').data([0]).join('g').attr('class', 'xaxis')
         .call(xAxis);
 
@@ -83,5 +89,23 @@ var generateViz = (data) => {
         .call(yAxis);
 
 }
+
+function onMouseOver(i, d) {
+    d3.select(this)
+      .style("fill", "yellow");
+    svg.append("text")
+        .attr("id", "idTexto")
+        .attr("x", 60)
+        .attr("y", 40)
+        .text("Insumos: " + d.valor);
+    //console.log("mouseover", d3.pointer(this))
+}  
+  
+function onMouseOut(d, i) {
+    d3.select(this)
+      .style("fill", "steelblue");
+    d3.select("#idTexto").remove();
+    //console.log("mouseover", d3.pointer(this))
+}  
 
 getData(_urlData)
